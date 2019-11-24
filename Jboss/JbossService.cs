3c1859d00088abfe08455a860c7d8963062af0a9
Alfonso Hernandez <alfonsohdez08@hotmail.com>
2019-11-24 12:16:57 -0500

    Add service status changing behavior

--- Jboss/JbossService.cs
+++ Jboss/JbossService.cs
@@ -8,18 +8,26 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Runtime.InteropServices;
 
 namespace JBoss
 {
     public partial class JBossStandaloneService : ServiceBase
     {
-        const string JbossHome = "JBOSS_HOME";
+        private const string JbossHome = "JBOSS_HOME";
+        private const int WaitTimeInSeconds = 30;
 
         private readonly string _jbossBinFolderPath;
+        private ServiceStatus _serviceStatus;
 
         public JBossStandaloneService()
         {
             _jbossBinFolderPath = Environment.GetEnvironmentVariable(JbossHome);
+            _serviceStatus = new ServiceStatus()
+            {
+                dwWaitHint = TimeSpan.FromSeconds(WaitTimeInSeconds).Milliseconds
+            };
+
             InitializeComponent();
         }
 
@@ -29,6 +37,8 @@ namespace JBoss
 
             try
             {
+                SetServiceStatus(ServiceState.SERVICE_START_PENDING);
+
                 var standaloneBatProcessInfo = new ProcessStartInfo(standaloneBatFile)
                 {
                     CreateNoWindow = true,
@@ -36,10 +46,14 @@ namespace JBoss
                 };
 
                 Process.Start(standaloneBatProcessInfo);
+
+                SetServiceStatus(ServiceState.SERVICE_RUNNING);
             }
             catch (Exception e)
             {
                 EventLog.WriteEntry($"Failed when attempting to start JBoss. Check the following message for more details: {e.Message}");
+
+                SetServiceStatus(ServiceState.SERVICE_STOPPED);
             }
 
         }
@@ -51,6 +65,8 @@ namespace JBoss
 
             try
             {
+                SetServiceStatus(ServiceState.SERVICE_STOP_PENDING);
+
                 var jbossCliBatProcessInfo = new ProcessStartInfo(jbossCliBatFile, shutdownArgs)
                 {
                     CreateNoWindow = true,
@@ -65,8 +81,41 @@ namespace JBoss
             {
                 EventLog.WriteEntry($"Failed when attempting to stop JBoss server. Check the following message for more details: {e.Message}");
             }
+            finally
+            {
+                SetServiceStatus(ServiceState.SERVICE_STOPPED);
+            }
 
         }
 
+        private void SetServiceStatus(ServiceState state)
+        {
+            _serviceStatus.dwCurrentState = state;
+            SetServiceStatus(this.ServiceHandle, ref _serviceStatus);
+        }
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
     }
+
+    enum ServiceState
+    {
+        SERVICE_STOPPED = 0x00000001,
+        SERVICE_START_PENDING = 0x00000002,
+        SERVICE_STOP_PENDING = 0x00000003,
+        SERVICE_RUNNING = 0x00000004,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct ServiceStatus
+    {
+        public int dwServiceType;
+        public ServiceState dwCurrentState;
+        public int dwControlsAccepted;
+        public int dwWin32ExitCode;
+        public int dwServiceSpecificExitCode;
+        public int dwCheckPoint;
+        public int dwWaitHint;
+    };
+
 }
